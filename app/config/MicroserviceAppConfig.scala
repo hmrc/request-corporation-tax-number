40beb181851f5012a0d76be55d378f6055689542
Oli Slade <oliver.slade@digital.hmrc.gov.uk>
2018-03-26 13:21:54 +0100

    Adding configs for open github

--- app/config/MicroserviceAppConfig.scala
+++ app/config/MicroserviceAppConfig.scala
@@ -1,17 +1,36 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package config
 
 import com.google.inject.{Inject, Singleton}
-import play.api.Configuration
-import uk.gov.hmrc.play.bootstrap.config.{AppName, BaseUrl}
+import play.api.{Configuration, Environment}
+import uk.gov.hmrc.play.bootstrap.config.AppName
+import uk.gov.hmrc.play.config.ServicesConfig
+
 
 @Singleton
-class MicroserviceAppConfig @Inject()(override val configuration: Configuration) extends AppName with BaseUrl {
+class MicroserviceAppConfig @Inject()(override val runModeConfiguration: Configuration, environment: Environment) extends ServicesConfig {
+  override protected def mode = environment.mode
 
-  private def loadConfig(key: String) = configuration.getString(key).getOrElse(throw new Exception(s"Missing configuration key: $key"))
+  private def loadConfig(key: String) = runModeConfiguration.getString(key).getOrElse(throw new Exception(s"Missing configuration key: $key"))
 
-  private def loadBoolean(key: String) = configuration.getBoolean(key).getOrElse(throw new Exception(s"Missing configuration key: $key"))
+  private def loadBoolean(key: String) = runModeConfiguration.getBoolean(key).getOrElse(throw new Exception(s"Missing configuration key: $key"))
 
-  private def loadMilliseconds(key : String, default : Long) = configuration.getMilliseconds(key).getOrElse(default)
+  private def loadMilliseconds(key : String, default : Long) = runModeConfiguration.getMilliseconds(key).getOrElse(default)
 
   lazy val fileUploadUrl: String = baseUrl("file-upload")
   lazy val fileUploadFrontendUrl: String = baseUrl("file-upload-frontend")
--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -30,15 +30,18 @@ object MicroServiceBuild extends Build with MicroService {
   private val mockitoAllVersion = "1.10.19"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "play-reactivemongo" % "6.2.0",
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.4.0"
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "1.4.0",
+    "uk.gov.hmrc" %% "domain" % "4.1.0",
+    "uk.gov.hmrc" %% "json-encryption" % "3.2.0",
+    "uk.gov.hmrc" %% "mongo-caching" % "5.0.0" exclude("uk.gov.hmrc","time_2.11")
   )
 
   def test(scope: String = "test,it") = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
     "org.scalatest" %% "scalatest" % "2.2.6" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
+    "org.jsoup" % "jsoup" % "1.7.3" % "test,it",
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusPlayVersion % scope,
     "org.mockito" % "mockito-all" % mockitoAllVersion % scope
