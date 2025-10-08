9765b040417a81814ea2101dc36138843ae41729
Victor Osipov <osipovvictor1994@gmail.com>
2025-10-08 10:15:52 +0100

    DDCE-7100 - revert bootstrap upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,15 +1,18 @@
+import play.sbt.PlayImport.ws
 import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlayVersion: String = "10.2.0"
+  private val bootstrapPlayVersion: String = "9.16.0"
   private val mongoVersion: String = "2.9.0"
 
   private val compile = Seq(
+    ws,
     "uk.gov.hmrc"            %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc"            %% "domain-play-30"            % "11.0.0",
     "org.apache.xmlgraphics" % "fop"                        % "2.11",
     "net.sf.saxon"           % "Saxon-HE"                   % "12.7",
+    "commons-io"             % "commons-io"                 % "2.20.0",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"       % mongoVersion
   )
 
