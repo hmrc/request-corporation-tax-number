8fbe77ed1e9d0535a263233242e5a85f4cde48fe
amruta-lohote <203861386+amruta-lohote@users.noreply.github.com>
2026-01-08 13:30:20 +0000

    DDCE-7812 - License header update for request-corporation-tax-number repo (#69)
    * DDCE-7812 - License header update for request-corporation-tax-number
    
    * DDCE-7812 - Dependency updated for request-corporation-tax-number

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlayVersion: String = "9.19.0"
-  private val mongoVersion: String = "2.10.0"
+  private val bootstrapPlayVersion: String = "10.5.0"
+  private val mongoVersion: String = "2.11.0"
 
   private val compile = Seq(
     ws,
